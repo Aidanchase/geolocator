86ab5f2e036fadbebd6949d70f11c338d2becdc0
Aidan <aidanmro@gmail.com>
2019-08-30 11:02:29 -0400

    added geolocator file

--- geolocator.js
+++ geolocator.js
@@ -9,21 +9,3 @@ function getCountry (){
 }
   
 export function getCountry;
-
-
-// <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-//   <script>
-//     function getCountry (){
-//   $.get("https://api.ipdata.co?api-key=test", function (response) {
-//       (JSON.stringify(response, null, 4));
-//       let countryName = response.country_name;
-//       if (countryName === "Canada"){
-//         $("#country-overlay").append("<div style="width: 50px; height: 50px; background-color: white; z-index: 1;" class=country-overlay-wrapper><div class='country-overlay'><h1 class='canadian'> We See You Are In Canada </h1> <p class='want-redirect'> Head Over To Our Canadian Store? </p> <button class='redirect-to-ca'>Cutshield.ca</button></div></div> ")
-//       }
-//   }, "jsonp");
-// }
-  
-//   	$(document).ready(function(){
-//     	getCountry()
-//     })
-//   </script>
